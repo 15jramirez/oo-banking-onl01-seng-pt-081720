01c0243083afe50317d197021acf6d11eb62dd37
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-30 04:18:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
      if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-
+       binding.pry
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
